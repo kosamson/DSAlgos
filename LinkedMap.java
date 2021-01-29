c058f6eac7dfe84a174e0f8467156f2e1a177e66
kosamson <kosamson1@gmail.com>
2021-01-28 19:33:40 -0800

    Start implementing Map interface in LinkedMap

--- LinkedMap.java
+++ LinkedMap.java
@@ -1,78 +1,106 @@
+import java.util.Map;
+
 /* Custom implementation of the Map interface using a linked-node structure. Performs most operations in O(n) time complexity. */
 
-public class LinkedMap<T1, T2> {
-    private MapNode<T1, T2> head;
+public class LinkedMap<keyType extends Comparable<keyType>, valueType extends Comparable<valueType>> implements Map<keyType, valueType> {
+    private MapNode<keyType, valueType> head;
+    private int size;
 
     public LinkedMap() {
         this.head = null;
     }
 
-    public void put(T1 inpKey, T2 inpVal) {
-        if (head == null)
-            head = new MapNode<T1, T2>(inpKey, inpVal);
+    public valueType put(keyType inpKey, valueType inpVal) {
+        this.size++;
+
+        if (head == null) {
+            head = new MapNode<keyType, valueType>(inpKey, inpVal);
+            return inpVal;
+        }
         
         else {
-            MapNode<T1, T2> iter = head;
+            MapNode<keyType, valueType> iter = head;
 
             while (iter.next != null) {
                 if (iter.key == inpKey) {
                     iter.value = inpVal;
-                    return;
+                    return inpVal;
                 }
 
                 iter = iter.next;
             }
 
-            iter.next = new MapNode<T1, T2>(inpKey, inpVal);
+            iter.next = new MapNode<keyType, valueType>(inpKey, inpVal);
+            return inpVal;
         }
     }
 
-    public void remove(T1 inpKey) {
-        if (head == null)
-            return;
-
-        if (head.key == inpKey) {
-            head = null;
-            return;
+    public valueType remove(keyType inpKey) {
+        if (head == null) {
+            this.size--;
+            return null;
         }
 
-        MapNode<T1, T2> iter = head;
+        if (head.key == inpKey) {
+            valueType tmp = head.value;
+            head = null;
+            this.size--;
+            return tmp;
+        }
+
+        MapNode<keyType, valueType> iter = head;
 
         while (iter.next != null) {
-            if (iter.next.key == inpKey) {
+            if (iter.next.key.equals(inpKey)) {
+                valueType tmp = iter.next.value;
                 iter.next = iter.next.next;
-                return;
+                this.size--;
+                return tmp;
             }
             
             iter = iter.next;
         }
+
+        throw new IllegalArgumentException("Item with key: " + inpKey + " not found in map.");
     }
 
-    public T2 get(T1 inpKey) {
-        MapNode<T1, T2> iter = head;
+    public valueType get(Object inpKey) {
+        MapNode<keyType, valueType> iter = head;
 
         while (iter != null) {
-            if (iter.key == inpKey)
+            if (iter.key.equals(inpKey))
                 return iter.value;
 
             iter = iter.next;
         }
 
-        return null;
+        throw new IllegalArgumentException("Item with key: " + inpKey + " not found in map.");
     }
-    
-    private class MapNode<T1, T2> {
-        public MapNode next;
-        public T1 key;
-        public T2 value;
 
-        public MapNode() {
-            this.next = null;
-            this.key = null;
-            this.value = null;
+    public valueType getOrDefault(keyType inpKey, valueType defaultValue) {
+        valueType retVal = null;
+
+        try {
+            retVal = get(inpKey);
         }
 
-        public MapNode(T1 inpKey, T2 inpVal) {
+        catch (IllegalArgumentException e) {
+            return defaultValue;
+        }
+
+        return retVal;
+    }
+
+    public int size() {
+        return this.size;
+    }
+    
+    private class MapNode<keyType, valueType> {
+        public MapNode<keyType, valueType> next;
+        public keyType key;
+        public valueType value;
+
+        public MapNode(keyType inpKey, valueType inpVal) {
             this.next = null;
             this.key = inpKey;
             this.value = inpVal;
