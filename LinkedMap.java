0af684f19affc0b6eb5d5f8adb15a2e4045870cb
kosamson <kosamson1@gmail.com>
2021-01-29 11:27:55 -0800

    Remove Map interface and fix warnings

--- LinkedMap.java
+++ LinkedMap.java
@@ -1,25 +1,27 @@
-import java.util.Map;
-
 /* Custom implementation of the Map interface using a linked-node structure. Performs most operations in O(n) time complexity. */
 
-public class LinkedMap<keyType extends Comparable<keyType>, valueType extends Comparable<valueType>> implements Map<keyType, valueType> {
-    private MapNode<keyType, valueType> head;
+public class LinkedMap<K extends Comparable<K>, V extends Comparable<V>> {
+    private MapNode<K, V> head;
     private int size;
 
     public LinkedMap() {
         this.head = null;
     }
 
-    public valueType put(keyType inpKey, valueType inpVal) {
+    public void clear() {
+        this.head = null;
+    }
+
+    public V put(K inpKey, V inpVal) {
         this.size++;
 
         if (head == null) {
-            head = new MapNode<keyType, valueType>(inpKey, inpVal);
+            head = new MapNode<K, V>(inpKey, inpVal);
             return inpVal;
         }
         
         else {
-            MapNode<keyType, valueType> iter = head;
+            MapNode<K, V> iter = head;
 
             while (iter.next != null) {
                 if (iter.key == inpKey) {
@@ -30,29 +32,29 @@ public class LinkedMap<keyType extends Comparable<keyType>, valueType extends Co
                 iter = iter.next;
             }
 
-            iter.next = new MapNode<keyType, valueType>(inpKey, inpVal);
+            iter.next = new MapNode<K, V>(inpKey, inpVal);
             return inpVal;
         }
     }
 
-    public valueType remove(keyType inpKey) {
+    public V remove(K inpKey) {
         if (head == null) {
             this.size--;
             return null;
         }
 
         if (head.key == inpKey) {
-            valueType tmp = head.value;
+            V tmp = head.value;
             head = null;
             this.size--;
             return tmp;
         }
 
-        MapNode<keyType, valueType> iter = head;
+        MapNode<K, V> iter = head;
 
         while (iter.next != null) {
             if (iter.next.key.equals(inpKey)) {
-                valueType tmp = iter.next.value;
+                V tmp = iter.next.value;
                 iter.next = iter.next.next;
                 this.size--;
                 return tmp;
@@ -64,8 +66,8 @@ public class LinkedMap<keyType extends Comparable<keyType>, valueType extends Co
         throw new IllegalArgumentException("Item with key: " + inpKey + " not found in map.");
     }
 
-    public valueType get(Object inpKey) {
-        MapNode<keyType, valueType> iter = head;
+    public V get(Object inpKey) {
+        MapNode<K, V> iter = head;
 
         while (iter != null) {
             if (iter.key.equals(inpKey))
@@ -77,8 +79,8 @@ public class LinkedMap<keyType extends Comparable<keyType>, valueType extends Co
         throw new IllegalArgumentException("Item with key: " + inpKey + " not found in map.");
     }
 
-    public valueType getOrDefault(keyType inpKey, valueType defaultValue) {
-        valueType retVal = null;
+    public V getOrDefault(K inpKey, V defaultValue) {
+        V retVal = null;
 
         try {
             retVal = get(inpKey);
@@ -95,12 +97,12 @@ public class LinkedMap<keyType extends Comparable<keyType>, valueType extends Co
         return this.size;
     }
     
-    private class MapNode<keyType, valueType> {
-        public MapNode<keyType, valueType> next;
-        public keyType key;
-        public valueType value;
+    private class MapNode<K1, V1> {
+        public MapNode<K1, V1> next;
+        public K1 key;
+        public V1 value;
 
-        public MapNode(keyType inpKey, valueType inpVal) {
+        public MapNode(K1 inpKey, V1 inpVal) {
             this.next = null;
             this.key = inpKey;
             this.value = inpVal;
